27321b2f291d76eb393d241e5aca7ce59940c89f
Jorge Capona <jcapona@gmail.com>
2021-05-20 23:05:12 +0100

    Display spinner while opening links in tour

--- src/frontend/src/pages/linksPage/LinksPage.tsx
+++ src/frontend/src/pages/linksPage/LinksPage.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import linkScreen from "../../assets/images/upgrade-page.png";
 import styles from "./LinksPage.module.css";
 import Layout from "../../components/layout/Layout";
 import Button from "../../components/atoms/button/Button";
+import Spinner from "../../components/atoms/spinner/Spinner";
+
 import closePtBrowser from "../../services/closePtBrowser";
 import openPythonSDKDocs from "../../services/openPythonSDKDocs";
 import openFurther from "../../services/openFurther";
@@ -18,6 +20,37 @@ export type Props = {
 
 export default ({ furtherUrl, isOnWebUi, pythonDocsUrl  }: Props) => {
   const kbUrl = "https://knowledgebase.pi-top.com";
+  const [isOpeningPythonDocs, setIsOpeningPythonDocs] = useState(false);
+  const [isOpeningFurther, setIsOpeningFurther] = useState(false);
+  const [isOpeningKnowledgeBase, setIsOpeningKnowledgeBase] = useState(false);
+
+  const goToFurther = () => {
+    if (isOnWebUi) {
+      openFurther();
+      setIsOpeningFurther(true);
+      window.setTimeout(() => setIsOpeningFurther(false), 8000);
+    } else {
+      window.open(furtherUrl);
+    }
+  }
+  const goToPythonSDKDocs = () => {
+    if (isOnWebUi) {
+      openPythonSDKDocs();
+      setIsOpeningPythonDocs(true);
+      window.setTimeout(() => setIsOpeningPythonDocs(false), 8000);
+    } else {
+      window.open(pythonDocsUrl);
+    }
+  }
+  const goToKB = () => {
+    if (isOnWebUi) {
+      openKnowledgeBase();
+      setIsOpeningKnowledgeBase(true);
+      window.setTimeout(() => setIsOpeningKnowledgeBase(false), 8000);
+    } else {
+      window.open(kbUrl);
+    }
+  }
 
   return (
     <Layout
@@ -41,15 +74,39 @@ export default ({ furtherUrl, isOnWebUi, pythonDocsUrl  }: Props) => {
       }}
       className={styles.root}
     >
-      <Button className={styles.linkButton} unstyled onClick={() => isOnWebUi? openFurther() : window.open(furtherUrl)}>
-        Go Further
-      </Button>
-      <Button className={styles.linkButton} unstyled onClick={() => isOnWebUi? openPythonSDKDocs() : window.open(pythonDocsUrl)}>
-        Checkout the Python SDK
-      </Button>
-      <Button className={styles.linkButton} unstyled onClick={() => isOnWebUi? openKnowledgeBase() : window.open(kbUrl)}>
-        Go to the Knowledge Base
-      </Button>
+      <div className={styles.buttonContainer}>
+        <Button
+          className={styles.linkButton}
+          unstyled
+          disabled={isOpeningFurther}
+          onClick={() => goToFurther()}>
+          Go Further
+        </Button>
+        <span className={styles.spinnerSpan}>{isOpeningFurther && <Spinner size={25} />}</span>
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <Button
+          className={styles.linkButton}
+          unstyled
+          disabled={isOpeningPythonDocs}
+          onClick={() => goToPythonSDKDocs()}>
+          Checkout the Python SDK
+        </Button>
+        <span className={styles.spinnerSpan}>{isOpeningPythonDocs && <Spinner size={25} />}</span>
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <Button
+          className={styles.linkButton}
+          unstyled
+          disabled={isOpeningKnowledgeBase}
+          onClick={() => goToKB()}>
+          Go to the Knowledge Base
+        </Button>
+        <span className={styles.spinnerSpan}>{isOpeningKnowledgeBase && <Spinner size={25} />}</span>
+      </div>
+
     </Layout>
   );
 };
