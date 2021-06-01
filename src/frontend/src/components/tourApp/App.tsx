37b365e91c9a3a97d75bb9b328297aff90d485ae
Jorge Capona <jcapona@gmail.com>
2021-06-01 18:19:08 -0400

    Remove build info from tour

--- src/frontend/src/components/tourApp/App.tsx
+++ src/frontend/src/components/tourApp/App.tsx
@@ -1,26 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import { Route, Switch } from "react-router-dom";
 
 import TourSplashPage from "../../pages/tourSplashPage/TourSplashPage";
 import LinksPage from "../../pages/linksPage/LinksPage";
 import ErrorPage from "../../pages/errorPage/ErrorPage";
-import BuildInformation from "../buildInformation/BuildInformation";
 
-import getBuildInfo from "../../services/getBuildInfo";
-
-import { BuildInfo } from "../../types/Build";
 import { PageRoute } from "../../types/Page";
 
 export default () => {
-  const [buildInfo, setBuildInfo] = useState<BuildInfo>();
-
-  useEffect(() => {
-    getBuildInfo()
-      .then(setBuildInfo)
-      .catch(() => null);
-  }, []);
-
   return (
     <>
       <Switch>
@@ -40,7 +28,6 @@ export default () => {
 
         <Route component={ErrorPage} />
       </Switch>
-      <BuildInformation info={buildInfo} />
     </>
   );
 };
