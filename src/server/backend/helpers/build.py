e6007ef149b3c74ee82a17a1cf7c620812239f6c
Jorge Capona <jcapona@gmail.com>
2021-08-09 16:35:45 -0400

    Use SDK to retrieve OS build information

--- src/server/backend/helpers/build.py
+++ src/server/backend/helpers/build.py
@@ -1,44 +1,18 @@
 from json import dumps
 
 from pitop.common.logger import PTLogger
-
-from .paths import pt_issue
+from pitop.common.pt_os import get_pitopOS_info
 
 
 def os_build_info():
     PTLogger.info("Function: os_build_info()")
+    build_info = get_pitopOS_info()
 
-    build_name = None
-    build_number = None
-    build_date = None
-    build_repo = None
-    final_repo = None
-    build_hash = None
-
-    with open(pt_issue()) as file:
-        for line in file:
-            if "Build Name:" in line:
-                build_name = line.split(":")[1].strip()
-            elif "Build Number:" in line:
-                build_number = line.split(":")[1].strip()
-            elif "Build Date:" in line:
-                build_date = line.split(":")[1].strip()
-            elif "Build Apt Repo:" in line:
-                build_repo = line.split(":")[1].strip()
-            elif "Final Apt Repo:" in line:
-                final_repo = line.split(":")[1].strip()
-            elif "Build Pipeline Commit Hash:" in line:
-                build_hash = line.split(":")[1].strip()
-
-    resp = {
-        "buildName": build_name,
-        "buildNumber": build_number,
-        "buildDate": build_date,
-        "buildRepo": build_repo,
-        "finalRepo": final_repo,
-        "buildHash": build_hash,
+    build = {
+        "buildRepo": build_info.build_type,
+        "buildDate": build_info.date,
+        "buildNumber": build_info.number,
+        "buildCommit": build_info.commit,
     }
-
-    PTLogger.info("OS build information: " + dumps(resp))
-
-    return resp
+    PTLogger.info("OS build information: " + dumps(build))
+    return build
