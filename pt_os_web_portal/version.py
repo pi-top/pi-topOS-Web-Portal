c50e37a28bc623d10e4bb295aa9f9cb67e6c940f
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-09-07 19:45:20 +0100

    Use version.txt

--- pt_os_web_portal/version.py
+++ pt_os_web_portal/version.py
@@ -1,1 +1,12 @@
-__version__ = "0.4.0"
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+__version__ = "N/A"
+try:
+    with open(f"{HERE}/version.txt", "r+") as f:
+        # Reading form a file
+        __version__ = f.read().strip()
+
+except Exception:
+    pass
