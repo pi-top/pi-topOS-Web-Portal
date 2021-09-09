2980fb81c5261e4c75fc77c12c26bc593b784a89
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-09-09 14:36:18 +0100

    WIP

--- pt_os_web_portal/os_updater.py
+++ pt_os_web_portal/os_updater.py
@@ -1,9 +1,11 @@
 from datetime import date, datetime
+from enum import Enum, auto
+from json import dumps as jdumps
 
 from pitop.common.logger import PTLogger
 
-from ..events import MessageType
 from .backend.helpers.extras import FWUpdaterBreadcrumbManager
+from .backend.helpers.os_update_manager import OSUpdateManager
 from .config_manager import ConfigManager
 from .event import post_event
 from .finalise import onboarding_completed
@@ -11,32 +13,118 @@ from .system_clock import is_system_clock_synchronized, synchronize_system_clock
 from .wifi_manager import is_connected_to_internet
 
 
+class MessageType(Enum):
+    ERROR = auto()
+    START = auto()
+    STATUS = auto()
+    FINISH = auto()
+
+
+class EventNames(Enum):
+    OS_UPGRADE = auto()
+    OS_PREPARE_UPGRADE = auto()
+    SIZE = auto()
+
+
+def create_emit_os_prepare_upgrade_message(ws):
+    def emit_os_prepare_upgrade_message(
+        message_type: MessageType, status_message: str, percent: float
+    ) -> None:
+        data = {
+            "type": EventNames.OS_PREPARE_UPGRADE.name,
+            "payload": {
+                "status": message_type.name,
+                "percent": percent,
+                "message": status_message.strip(),
+            },
+        }
+        PTLogger.info(str(data))
+        ws.send(jdumps(data))
+
+    return emit_os_prepare_upgrade_message
+
+
+def create_emit_os_upgrade_message(ws):
+    def emit_os_upgrade_message(
+        message_type: MessageType, status_message: str, percent: float
+    ) -> None:
+        data = {
+            "type": EventNames.OS_UPGRADE.name,
+            "payload": {
+                "status": message_type.name,
+                "percent": percent,
+                "message": status_message.strip(),
+            },
+        }
+        PTLogger.info(str(data))
+        ws.send(jdumps(data))
+
+    return emit_os_upgrade_message
+
+
+def create_emit_os_size_message(ws):
+    def emit_os_size_message(message_type, size):
+        data = {
+            "type": EventNames.SIZE.name,
+            "payload": {"size": size, "status": message_type.name},
+        }
+        PTLogger.info(str(data))
+        ws.send(jdumps(data))
+
+    return emit_os_size_message
+
+
 class OSUpdater:
     def __init__(self):
-        # self.manager = OSUpdateManager()
+        self.manager = OSUpdateManager()
         pass
 
     def prepare_os_upgrade(self, ws=None):
         if not is_system_clock_synchronized():
             synchronize_system_clock()
 
-        post_event("os_updater_prepare_started", True)
+        post_event("os_updater_prepare", "started")
 
-    def os_upgrade_size(self, callback):
+        callback = create_emit_os_prepare_upgrade_message(ws) if ws else None
         try:
-            callback(
-                MessageType.STATUS,
-                {
-                    "downloadSize": self.manager.download_size(),
-                    "requiredSpace": self.manager.required_space(),
-                },
-            )
+            if callable(callback):
+                callback(MessageType.START, "Preparing OS upgrade", 0.0)
+
+            self.manager.update(callback)
+            self.manager.stage_upgrade(callback)
+
+            if self.manager.cache.install_count == 0:
+                self.manager.update_last_check_config()
+
+            if callable(callback):
+                callback(MessageType.FINISH, "Finished preparing", 100.0)
+
+            post_event("os_updater_prepare", "success")
+        except Exception as e:
+            post_event("os_updater_prepare", "failed")
+
+            if callable(callback):
+                callback(MessageType.ERROR, f"{e}", 0.0)
+
+    def os_upgrade_size(self, ws=None):
+        callback = create_emit_os_size_message(ws) if ws else None
+        try:
+            if callable(callback):
+                callback(
+                    MessageType.STATUS,
+                    {
+                        "downloadSize": self.manager.download_size(),
+                        "requiredSpace": self.manager.required_space(),
+                    },
+                )
         except Exception as e:
             PTLogger.info(f"os_upgrade_size: {e}")
-            callback(MessageType.ERROR, {"downloadSize": 0, "requiredSpace": 0})
+            if callable(callback):
+                callback(MessageType.ERROR, {"downloadSize": 0, "requiredSpace": 0})
 
-    def start_os_upgrade(self, callback):
+    def start_os_upgrade(self, ws=None):
         fw_breadcrumb_manager = FWUpdaterBreadcrumbManager()
+        callback = create_emit_os_upgrade_message(ws) if ws else None
         try:
             # tell firmware updater updater not to timeout
             if not fw_breadcrumb_manager.is_ready():
@@ -48,7 +136,8 @@ class OSUpdater:
             self.manager.upgrade(callback)
             self.manager.update_last_check_config()
         except Exception as e:
-            callback(MessageType.ERROR, f"{e}", 0.0)
+            if callable(callback):
+                callback(MessageType.ERROR, f"{e}", 0.0)
         finally:
             fw_breadcrumb_manager.set_ready("pt-os-web-portal: Finished update.")
             # Tell firmware updater to no longer block on extended timeout
@@ -58,7 +147,7 @@ class OSUpdater:
                 )
                 fw_breadcrumb_manager.clear_extend_timeout()
 
-    def should_check_for_updates(self):
+    def should_check_for_updates(self, ws=None):
         if not onboarding_completed():
             PTLogger.info("Onboarding not completed yet, skipping update check...")
             return False
@@ -83,12 +172,12 @@ class OSUpdater:
 
         return should
 
-    def updates_available(self):
+    def updates_available(self, ws=None):
         self.prepare_os_upgrade()
         # self.wait_for_prepare_to_finish()
         return self.manager.cache.install_count > 0
 
-    def do_update_check(self):
+    def do_update_check(self, ws=None):
         if self.should_check_for_updates():
             PTLogger.info("Checking for updates...")
 
