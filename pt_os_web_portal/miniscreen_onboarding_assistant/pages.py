aed5762e9746cdf4a6e64b4f662947ad7738b765
m-roberts <m-roberts@users.noreply.github.com>
2021-09-20 17:09:35 +0100

    Get hostname dynamically

--- pt_os_web_portal/miniscreen_onboarding_assistant/pages.py
+++ pt_os_web_portal/miniscreen_onboarding_assistant/pages.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+from subprocess import run
 from time import perf_counter
 
 from PIL import Image, ImageDraw, ImageFont
@@ -265,7 +266,8 @@ class OpenBrowserPage(PageBase):
         txt = "Waiting for\nconnection..."
 
         if self.has_connected_device or self.is_connected_to_internet:
-            txt = "Open browser to\nhttp://pi-top.local\nor\nhttp://192.168.64.1"
+            hostname = run("hostname").stdout
+            txt = f"Open browser to\nhttp://{hostname}.local\nor\nhttp://192.168.64.1"
 
         return txt
 
