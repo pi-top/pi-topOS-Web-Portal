7e9cbb33c5a81ca7dfb70ac8846e2a9b36d62788
m-roberts <m-roberts@users.noreply.github.com>
2021-10-05 17:01:49 +0100

    Update URL

--- frontend/src/pages/linksPage/LinksPage.tsx
+++ frontend/src/pages/linksPage/LinksPage.tsx
@@ -21,7 +21,7 @@ import { runningOnWebRenderer } from "../../helpers/utils";
 export default () => {
   const kbUrl = "https://knowledgebase.pi-top.com";
   const forumUrl = "https://forum.pi-top.com";
-  const [pythonDocsUrl, setPythonDocsUrl] = useState("https://docs.pi-top.com");
+  const [pythonDocsUrl, setPythonDocsUrl] = useState("https://docs.pi-top.com/python-sdk/");
   const [furtherUrl, setFurtherUrl] = useState("https://further.pi-top.com/start");
   const [isOpeningLink, setIsOpeningLink] = useState(false);
 
