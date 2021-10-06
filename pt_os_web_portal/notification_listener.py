b792cb77edb305f31e7283946a72e67d8f8e82f9
m-roberts <m-roberts@users.noreply.github.com>
2021-10-06 15:01:52 +0100

    Detect if an app window is open, add update notification if window closed

--- pt_os_web_portal/notification_listener.py
+++ pt_os_web_portal/notification_listener.py
@@ -1,5 +1,6 @@
 from pitop.common.notifications import send_notification
 
+from .app_window import OsUpdaterAppWindow
 from .event import AppEvents, subscribe
 
 
@@ -13,5 +14,23 @@ def handle_os_has_updates_event(os_has_updates):
         )
 
 
+def handle_os_has_finished_update(state):
+    if state == "started":
+        return
+
+    if state == "failed":
+        # Not yet implemented
+        return
+
+    if not OsUpdaterAppWindow().is_open():
+        send_notification(
+            title="pi-topOS Software Updater",
+            text="Updates were applied successfully!",
+            timeout=0,
+            icon_name="system-software-update",
+        )
+
+
 def setup_notification_event_handlers():
     subscribe(AppEvents.OS_HAS_UPDATES, handle_os_has_updates_event)
+    subscribe(AppEvents.OS_UPDATER_UPGRADE, handle_os_has_finished_update)
