07a883f7d204348cc2e4b20f51199aa78d088d34
Jorge Capona <jcapona@gmail.com>
2022-01-21 15:12:22 -0300

    Mock dependency modules when running tests

--- pt_os_web_portal/conftest.py
+++ pt_os_web_portal/conftest.py
@@ -1,18 +1,46 @@
 import os
 from subprocess import Popen
+from sys import modules
 from time import sleep
+from unittest.mock import Mock
 
 import pytest
 
-from .backend import create_app
 from .tests.data.finalise_data import cmd_line_before
 from .tests.data.keyboard_data import keyboard_file_before
 
 
+@pytest.fixture(autouse=True)
+def patch_modules():
+    modules_to_patch = [
+        "further_link.start_further",
+        "pitop",
+        "pitop.battery",
+        "pitop.common.command_runner",
+        "pitop.common.common_ids",
+        "pitop.common.common_names",
+        "pitop.common.current_session_info",
+        "pitop.common.firmware_device",
+        "pitop.common.notifications",
+        "pitop.common.pt_os",
+        "pitop.common.sys_info",
+        "pitop.miniscreen.oled.assistant",
+        "pitop.system",
+        "pt_fw_updater.update",
+    ]
+    for module in modules_to_patch:
+        modules[module] = Mock()
+
+    yield
+
+    for module in modules_to_patch:
+        del modules[module]
+
+
 @pytest.fixture(scope="session")
 def app():
-    print("__________________________________")
-    print("__________________________________")
+    from .backend import create_app
+
     app = create_app(test_mode=True, os_updater=None)
     testing_client = app.test_client()
     ctx = app.app_context()
@@ -59,27 +87,27 @@ def restore_files():
 
 @pytest.fixture(scope="function")
 def wifi_manager_module():
-    import sys
-
     import pt_os_web_portal.backend.helpers.wifi_manager
+
+    from .backend import create_app
 
     create_app(test_mode=True, os_updater=None)
     yield pt_os_web_portal.backend.helpers.wifi_manager
 
-    if "pt_os_web_portal.backend.helpers.wifi_manager" in sys.modules:
-        del sys.modules["pt_os_web_portal.backend.helpers.wifi_manager"]
+    if "pt_os_web_portal.backend.helpers.wifi_manager" in modules:
+        del modules["pt_os_web_portal.backend.helpers.wifi_manager"]
     del pt_os_web_portal.backend.helpers.wifi_manager
 
 
 @pytest.fixture(scope="function")
 def os_updater_module():
-    import sys
-
     import pt_os_web_portal.backend.helpers.os_updater
+
+    from .backend import create_app
 
     create_app(test_mode=True, os_updater=None)
     yield pt_os_web_portal.backend.helpers.os_updater
 
-    if "pt_os_web_portal.backend.helpers.os_updater" in sys.modules:
-        del sys.modules["pt_os_web_portal.backend.helpers.os_updater"]
+    if "pt_os_web_portal.backend.helpers.os_updater" in modules:
+        del modules["pt_os_web_portal.backend.helpers.os_updater"]
     del pt_os_web_portal.backend.helpers.os_updater
