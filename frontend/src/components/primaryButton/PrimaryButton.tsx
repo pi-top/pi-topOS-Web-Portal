8ef573a569acc1fbda5661fa7429daf7f83992cb
zz-james <zz.james@gmail.com>
2022-02-08 17:34:22 +0000

    fix: off by one problem on firefox on mac

--- frontend/src/components/primaryButton/PrimaryButton.tsx
+++ frontend/src/components/primaryButton/PrimaryButton.tsx
@@ -31,62 +31,45 @@ const PrimaryButton = ({
       type={type}
       unstyled
     >
-      <svg
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        className={styles.primaryButtonSVGContainer}
-      >
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" className={styles.primaryButtonSVGContainer}>
         <defs>
-          <symbol
-            id="corner"
-            width="100"
-            height="50"
-            viewBox="0 0 100 50"
-            className={styles.rolloverFill}
-          >
-            <path
-              vectorEffect="non-scaling-stroke"
-              shapeRendering="geometricPrecision"
-              d="
-                M 100 92
-                L 86.74 92
-                C 49.9 90.713 31.68 87.629 19.35 76.94
-                C 1.11 61.325 1.95 34.495 21.14 19.875
-                C 36.5 7.998 61.45 4.885 86.74 5
-                L 100 5
-                "
-              className={styles.border}
-            />
+          <symbol id="corner" viewBox="0 0 38 17" preserveAspectRatio="xMaxYMin slice" className={styles.rolloverFill}>
+            <path d="
+            M38 32 30.192 32
+            C17.754 31.543 11.604 30.481 7.442 26.797 1.285 21.415 1.568 12.167 8.047 7.128 13.231 3.034 21.653 1.962 30.192 2.001
+            L38 2" className={styles.border} vectorEffect="non-scaling-stroke" shapeRendering="geometricPrecision" />
           </symbol>
 
-          <symbol id="topBottom" viewBox="0 0 270 4" preserveAspectRatio="none">
+          <symbol id="topBottom" viewBox="0 0 100 2" preserveAspectRatio="none" className={styles.rolloverFill}>
             <path
               vectorEffect="non-scaling-stroke"
-              shapeRendering="geometricPrecision"
-              d="m 0 2 l 270 0"
+              shapeRendering="crispEdges"
+              d="m 0 1 l 100 0"
+              className={styles.border}
             />
           </symbol>
 
           <symbol
             id="leftRight"
-            viewBox="0 0 80 7.6"
-            preserveAspectRatio="none"
+            viewBox="0 0 100 2"
+            preserveAspectRatio='xMinYMax slice'
           >
-            <use href="#topBottom" transform="rotate(90)" x="0" y="-4.7" />
+            <use href="#topBottom" transform="rotate(90)" x="0" y="-55%" />
           </symbol>
+
         </defs>
 
         {/* <!-- the corners: --> */}
 
         {/* <!-- 1. top-left: --> */}
-        <use href="#corner" width="40" height="15" />
+        <use width="38" height="17" href="#corner" />
 
         {/* <!-- 2. top-right: --> */}
         <use
           href="#corner"
           transform="scale(-1, 1)"
           width="40"
-          height="15"
+          height="17"
           x="-100%"
           y="0"
         />
@@ -97,7 +80,7 @@ const PrimaryButton = ({
           transform="scale(1, -1)"
           style={{ overflow: 'visible' }}
           width="40"
-          height="15"
+          height="17"
           x="0"
           y="-100%"
         />
@@ -107,7 +90,7 @@ const PrimaryButton = ({
           href="#corner"
           transform="scale(-1, -1)"
           width="40"
-          height="15"
+          height="17"
           x="-100%"
           y="-100%"
         />
@@ -115,62 +98,66 @@ const PrimaryButton = ({
         {/* <!-- the edges: --> */}
 
         {/* 5 top: */}
-        <foreignObject x="0" y="0" width="100%" height="10px">
+
+        <foreignObject x="0" y="1" width="100%" height="2px">
           <span
             style={{
               display: 'flex',
               lineHeight: 0,
-              margin: '0.4px 34.5px 0 34.9px',
+              margin: '0px 35px 0 35px',
             }}
             className={styles.rolloverBG}
           >
             <svg
               width="100%"
-              height="10"
+              height="2"
               preserveAspectRatio="none"
               version="1.1"
             >
               <use
                 href="#topBottom"
-                className={cx(styles.border)}
+                strokeWidth={2}
+                stroke="green"
                 width="100%"
                 height="2"
                 x="0"
-                y="0.3"
+                y="0"
               />
             </svg>
           </span>
         </foreignObject>
+
 
         {/* 6 bottom: */}
         <foreignObject
           x="0"
           y="-100%"
           width="100%"
-          height="10px"
+          height="4px"
           transform="scale(1, -1)"
         >
           <span
-            style={{ display: 'block', lineHeight: 0, margin: '0 34.9px' }}
+            style={{ display: 'block', lineHeight: 0, margin: '0 35px 0 35px' }}
             className={styles.rolloverBG}
           >
             <svg
               width="100%"
-              height="10"
+              height="4"
               preserveAspectRatio="none"
               version="1.1"
             >
               <use
                 href="#topBottom"
-                className={cx(styles.border)}
+                className={cx(styles.border, styles.fix)}
                 width="100%"
-                height="10"
+                height="2"
                 x="0"
-                y="-3.8"
+                y="0"
               />
             </svg>
           </span>
         </foreignObject>
+
 
         {/* 7 left: */}
         <foreignObject x="0" y="0" width="50px" height="100%">
@@ -178,25 +165,25 @@ const PrimaryButton = ({
             style={{
               display: 'flex',
               position: 'absolute',
-              left: '6px',
-              top: '14.7px',
-              bottom: '14.7px',
-              width: '20px',
+              left: '0',
+              top: '17px',
+              bottom: '17px',
+              width: '40px',
             }}
             className={styles.rolloverBG}
           >
             <svg
-              width="50px"
+              width="4"
               height="100%"
               preserveAspectRatio="none"
               version="1.1"
             >
               <use
                 href="#leftRight"
-                className={cx(styles.border)}
-                width="140"
-                height="100%"
-                x="-1px"
+                className={cx(styles.border, styles.fix2)}
+                width="4"
+                height="50"
+                x="1"
                 y="0"
               />
             </svg>
@@ -210,9 +197,9 @@ const PrimaryButton = ({
               display: 'flex',
               position: 'absolute',
               left: '20px',
-              right: '5px',
-              top: '14.7px',
-              bottom: '14.7px',
+              right: '1px',
+              top: '17px',
+              bottom: '17px',
             }}
             className={styles.rolloverBG}
           >
@@ -235,8 +222,6 @@ const PrimaryButton = ({
           </span>
         </foreignObject>
 
-        {/* 9 centre */}
-        {/* not used */}
       </svg>
       <span className={cx(styles.textWrapper, styles.rolloverFill)}>
         {children}
