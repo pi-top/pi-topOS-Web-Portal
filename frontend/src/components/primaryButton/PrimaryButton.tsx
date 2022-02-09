17eed70ab393b06e7c872d9ebf4663ca7c17e220
zz-james <zz.james@gmail.com>
2022-02-09 17:41:30 +0000

    fix: stoke alignment of corner

--- frontend/src/components/primaryButton/PrimaryButton.tsx
+++ frontend/src/components/primaryButton/PrimaryButton.tsx
@@ -81,7 +81,7 @@ const PrimaryButton = ({
           style={{ overflow: 'visible' }}
           width="40"
           height="17"
-          x="0"
+          x="-0.2"
           y="-100%"
         />
 
@@ -138,7 +138,6 @@ const PrimaryButton = ({
         >
           <span
             style={{ display: 'flex', lineHeight: 0, margin: '0 35px 0 35px' }}
-            className={styles.rolloverBG}
           >
             <svg
               width="100%"
