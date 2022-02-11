7047e0585be42130b2f2e9cbfb9f75aa9ded64da
Jorge Capona <jcapona@gmail.com>
2022-02-11 08:41:45 -0300

    Remove ListenerManager class

--- pt_os_web_portal/listener_manager.py
+++
@@ -1,6 +0,0 @@
-from .device_registration.listener import setup_device_registration_event_handlers
-
-
-class ListenerManager:
-    def start(self):
-        setup_device_registration_event_handlers()
