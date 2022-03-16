bc9fe169f7687ca6f5817168e8dd1ba6b2ea16d6
Jorge Capona <jcapona@gmail.com>
2022-03-16 19:55:19 -0300

    WIP: Add device registration module tests

--- pt_os_web_portal/device_registration/functions.py
+++ pt_os_web_portal/device_registration/functions.py
@@ -1,5 +1,6 @@
 import logging
 from time import sleep
+from typing import Dict, Tuple
 
 import requests
 from pitop.common.pt_os import get_pitopOS_info
@@ -13,7 +14,7 @@ logger = logging.getLogger(__name__)
 API_ENDPOINT = "https://backend.pi-top.com/utils/v1/device/register"
 
 
-def send_data_and_get_resp(data):
+def send_data_and_get_resp(data) -> Tuple:
     headers = {"Content-Type": "application/json", "Accept": "application/json"}
     try:
         logger.debug("POSTing data...")
@@ -24,39 +25,36 @@ def send_data_and_get_resp(data):
     return None, None
 
 
-def device_is_registered():
+def device_is_registered() -> bool:
     return state.get("registration", "is_registered") == "true"
 
 
-def create_device_registered_breadcrumb():
-    state.set("registration", "is_registered", "true")
-
-
-def send_register_device_request():
-
+def _get_registration_data() -> Dict:
     logger.debug("Getting device data to send...")
     build_info = get_pitopOS_info()
-
     data = {
         "serialNumber": serial_number(),
         "email": state.get("registration", "email"),
         "privacyAgreement": True,
         "device": device_type(),
-        "osVersion": build_info.build_run_number + "-" + build_info.build_date,
+        "osVersion": f"{build_info.build_run_number}-{build_info.build_date}",
         "updateRepo": build_info.build_type,
     }
 
     logger.info(f"Device information to register: {data}")
+    return data
+
+
+def send_register_device_request() -> None:
+    data = _get_registration_data()
 
     logger.debug("Attempting to send data to server...")
-    successfully_send_data = False
-
-    while not successfully_send_data:
-
+    successfully_sent_data = False
+    while not successfully_sent_data:
         statusCode, response = send_data_and_get_resp(data)
 
         if statusCode == 200 and response is not None and response["success"]:
-            successfully_send_data = True
+            successfully_sent_data = True
             logger.info("Successfully registered device")
         else:
             logger.warning(
@@ -67,4 +65,4 @@ def send_register_device_request():
             sleep(30)
 
     logger.info("Creating breadcrumb to avoid registering again")
-    create_device_registered_breadcrumb()
+    state.set("registration", "is_registered", "true")
