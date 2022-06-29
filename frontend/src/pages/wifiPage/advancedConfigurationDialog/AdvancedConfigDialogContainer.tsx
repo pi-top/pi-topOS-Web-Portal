e2e39e040f254100e4258646726cfc30dbe8e5d1
Jorge Capona <jcapona@gmail.com>
2022-06-29 16:38:27 -0400

    Create AdvancedConfigDialogContainer

---
+++ frontend/src/pages/wifiPage/advancedConfigurationDialog/AdvancedConfigDialogContainer.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import getVncWpaGuiUrl from "../../../services/getVncWpaGuiUrl";
+import startVncWpaGui from "../../../services/startVncWpaGui";
+import stopVncWpaGui from "../../../services/stopVncWpaGui";
+
+import AdvancedConfigDialog from "./AdvancedConfigDialog";
+
+export type Props = {
+  active: boolean;
+  onClose: () => void;
+};
+
+export default ({ ...props }: Props) => {
+  const [url, setUrl] = useState("");
+  const [error, setError] = useState(false);
+
+  const waitForUrlTimeout = 700;
+
+  const waitForUrl = () => {
+    const interval = setInterval(async () => {
+      try {
+        const data = await getVncWpaGuiUrl();
+        if (data.url !== "") {
+          clearInterval(interval);
+          setUrl(data.url);
+        }
+      } catch (_) {}
+    }, waitForUrlTimeout);
+  }
+
+  useEffect(() => {
+    if (props.active) {
+      startAdvancedWifiConfig();
+    };
+  }, [props.active]);
+
+  const startAdvancedWifiConfig = () => {
+    startVncWpaGui()
+      .then(() => setTimeout(waitForUrl, 300))
+      .catch(() => setError(true))
+  };
+
+  const stopAdvancedWifiConfig = () => {
+    setUrl("")
+    stopVncWpaGui()
+      .catch(() => setError(true))
+  };
+
+  return (
+    <AdvancedConfigDialog
+      active={props.active}
+      url={url}
+      onClose={() => {
+          stopAdvancedWifiConfig();
+          props.onClose();
+        }
+      }
+      error={error}
+    />
+  );
+};
