165c112c8ca17682a6a00ed158f5cd0710142a20
Jorge Capona <jcapona@users.noreply.github.com>
2022-07-04 09:40:31 -0400

    Replace skip page with instructions url (#389)
    * SkipPage -> DetailedInstructionsPage
    
    * Use SDK helper function to get device IP
    
    * SkipPage -> DetailedInstructionsPage
    
    * Use SDK helper function to get device IP
    
    * Update IP page using get_text utility

--- pt_os_web_portal/miniscreen_onboarding_assistant/pages/menu.py
+++ pt_os_web_portal/miniscreen_onboarding_assistant/pages/menu.py
@@ -2,7 +2,7 @@ import logging
 
 from pitop.battery import Battery
 from pitop.common.pt_os import get_pitopOS_info
-from pitop.common.sys_info import NetworkInterface, get_internal_ip
+from pitop.common.sys_info import get_pi_top_ip
 from pt_miniscreen.core import Component
 from pt_miniscreen.core.components import Text
 from pt_miniscreen.core.utils import apply_layers, layer, rectangle
@@ -17,13 +17,14 @@ SIZE = (128, 64)
 TEXT_POS = (0, 0)
 
 
-class SkipPage(Component):
+class DetailedInstructionsPage(Component):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.text_component = self.create_child(
             Text,
-            text="Skip pi-top connection guide?",
+            text="Are you stuck?\nGo to\npi-top.com/start-4",
+            wrap=False,
             font_size=FONT_SIZE,
             align="center",
             vertical_align="center",
@@ -158,23 +159,12 @@ class NetworksPage(Component):
 
         self.text_component = self.create_child(
             Text,
-            text=self.text,
+            text=f"IP address:\n{get_pi_top_ip()}",
             font_size=FONT_SIZE,
             align="center",
             vertical_align="center",
             fill=0,
         )
-
-    @property
-    def text(self):
-        ips = list()
-
-        for iface in NetworkInterface:
-            ip = get_internal_ip(iface.name)
-            if ip.replace("Internet Addresses Not Found", ""):
-                ips.append(ip)
-
-        return "\n".join(ips)
 
     def render(self, image):
         return apply_layers(
