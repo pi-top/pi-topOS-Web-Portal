a3a1828a236403e9cbcd6d3f94183e01ca823301
Jorge Capona <jcapona@gmail.com>
2022-07-26 16:18:34 -0400

    Update tests

--- tests/test_vnc_wpa_gui.py
+++ tests/test_vnc_wpa_gui.py
@@ -34,7 +34,7 @@ def test_wpa_gui_vnc_stops_if_no_clients_are_connected(app, mocker):
         return_value="",
     )
     mocker.patch(
-        "pt_os_web_portal.backend.routes.vnc_wpa_gui_clients",
+        "pt_os_web_portal.backend.routes.vnc_clients",
         return_value=0,
     )
 
@@ -55,62 +55,45 @@ def test_wpa_gui_vnc_doesnt_stop_if_clients_are_connected(app, mocker):
         "pt_os_web_portal.backend.helpers.system.run_command",
         return_value="",
     )
+
     mocker.patch(
-        "pt_os_web_portal.backend.routes.vnc_wpa_gui_clients",
+        "pt_os_web_portal.backend.routes.vnc_clients",
         return_value=1,
     )
 
     response = app.post("/stop-vnc-wpa-gui", json={})
-    assert run_command_mock.call_count == 0
     assert response.status_code == 200
     assert response.data == b"OK"
+    assert run_command_mock.call_count == 0
 
 
-def test_wpa_gui_vnc_url_empty(app, mocker):
-    run_command_mock = mocker.patch(
-        "pt_os_web_portal.backend.helpers.vnc.run_command",
+def test_wpa_gui_vnc_url_fails(app, mocker):
+    connection_details_mock = mocker.patch(
+        "pt_os_web_portal.backend.routes.vnc_connection_details",
         side_effect=Exception("oh oh"),
     )
 
     response = app.get("/vnc-wpa-gui-url")
 
-    run_command_mock.assert_called_once_with(
-        "/usr/bin/pt-web-vnc url --display-id 99", check=True, timeout=10
-    )
+    connection_details_mock.assert_called_once_with(99)
 
     assert response.status_code == 200
     assert response.data == b'{"url": ""}'
 
 
 def test_wpa_gui_vnc_url_with_content(app, mocker):
-    run_command_mock = mocker.patch(
-        "pt_os_web_portal.backend.helpers.vnc.run_command",
-        return_value="http://pi-top.com",
+    class DetailsMock:
+        url = "pi-top.com"
+        scheme = "http"
+        port = "2112"
+        path = "/blah"
+
+    mocker.patch(
+        "pt_os_web_portal.backend.routes.vnc_connection_details",
+        return_value=DetailsMock,
     )
 
     response = app.get("/vnc-wpa-gui-url")
 
-    run_command_mock.assert_called_once_with(
-        "/usr/bin/pt-web-vnc url --display-id 99", check=True, timeout=10
-    )
-
     assert response.status_code == 200
-    assert response.data == b'{"url": "http://pi-top.com"}'
-
-
-def test_wpa_gui_vnc_clients_command(mocker):
-    run_command_mock = mocker.patch(
-        "pt_os_web_portal.backend.helpers.vnc.run_command",
-        return_value="",
-    )
-    from pt_os_web_portal.backend.helpers.vnc import vnc_wpa_gui_clients
-
-    vnc_wpa_gui_clients()
-
-    assert run_command_mock.call_count == 1
-    run_command_mock.assert_called_once_with(
-        "/usr/bin/pt-web-vnc clients --display-id 99",
-        check=True,
-        timeout=10,
-        log_errors=False,
-    )
+    assert response.data == b'{"url": "http://localhost:2112/blah"}'
