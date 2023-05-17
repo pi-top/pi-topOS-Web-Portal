66fed0ca40524736355e0ab2c917793a178f201e
Jorge Capona <jcapona@gmail.com>
2023-05-17 16:22:15 -0400

    Onboarding assistant restarts miniscreen on error

--- pt_os_web_portal/miniscreen_onboarding_assistant/onboarding_assistant_app.py
+++ pt_os_web_portal/miniscreen_onboarding_assistant/onboarding_assistant_app.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 
 from pitop.system.pitop import Pitop
 from pt_miniscreen.core import App as BaseApp
@@ -9,32 +10,48 @@ logger = logging.getLogger(__name__)
 
 
 class OnboardingAssistantApp(BaseApp):
-    def __init__(self):
-        self.miniscreen = Pitop().miniscreen
+    miniscreen = None
 
-        self.miniscreen.select_button.when_released = self.handle_select_button_release
-        self.miniscreen.cancel_button.when_released = self.handle_cancel_button_release
-        self.miniscreen.up_button.when_released = self.handle_up_button_release
-        self.miniscreen.down_button.when_released = self.handle_down_button_release
+    def __init__(self):
+        self._configure_miniscreen()
 
         super().__init__(
-            display=self.miniscreen.device.display,
+            display=self.display_func,
             Root=RootComponent,
             size=self.miniscreen.size,
         )
 
-    def handle_select_button_release(self):
-        self.root.handle_select_button()
-
-    def handle_cancel_button_release(self):
-        self.root.switch_menu()
-
-    def handle_up_button_release(self):
-        self.root.scroll_up()
-
-    def handle_down_button_release(self):
-        self.root.scroll_down()
+    def _configure_miniscreen(self):
+        self.miniscreen = Pitop().miniscreen
+        self.miniscreen.select_button.when_released = self._create_button_handler(
+            lambda: self.root.handle_select_button()
+        )
+        self.miniscreen.cancel_button.when_released = self._create_button_handler(
+            lambda: self.root.switch_menu()
+        )
+        self.miniscreen.up_button.when_released = self._create_button_handler(
+            lambda: self.root.scroll_up()
+        )
+        self.miniscreen.down_button.when_released = self._create_button_handler(
+            lambda: self.root.scroll_down()
+        )
 
     @property
-    def user_has_control(self) -> bool:
-        return self.miniscreen.is_active
+    def display_func(self):
+        return self.miniscreen.device.display
+
+    def _create_button_handler(self, func):
+        def handler():
+            try:
+                if callable(func):
+                    func()
+            except Exception as e:
+                logger.error(f"Error in button handler: {e}")
+                traceback.print_exc()
+                self._configure_miniscreen()
+
+        return handler
+
+    @property
+    def user_has_control(self):
+        return hasattr(self.miniscreen, "is_active") and self.miniscreen.is_active
