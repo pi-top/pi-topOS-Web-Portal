cc58eea550d64ef738e592e9d66ad075f1a8db79
Jorge Capona <jcapona@gmail.com>
2023-07-03 10:49:32 -0400

    Don't use app context when setting registration email

--- pt_os_web_portal/backend/helpers/registration.py
+++ pt_os_web_portal/backend/helpers/registration.py
@@ -1,7 +1,5 @@
 import logging
 
-from flask import current_app
-
 from ... import state
 
 logger = logging.getLogger(__name__)
@@ -9,5 +7,5 @@ logger = logging.getLogger(__name__)
 
 def set_registration_email(email):
     logger.info(f"Function: set_registration_email(email='{email}')")
-    with current_app.app_context():
-        state.set("registration", "email", email)
+    state.set("registration", "email", email)
+    state.set("registration", "is_registered", "false")
