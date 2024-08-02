c034bdbe61cf8fbd896e2427d841e0228c43dcd0
Jorge Capona <jcapona@gmail.com>
2024-08-02 08:47:30 -0400

    Patch std library before everything else

--- pt_os_web_portal/__init__.py
+++ pt_os_web_portal/__init__.py
@@ -1,9 +1,11 @@
-import pitop.common.ptdm
-import zmq.green
 from gevent import monkey
 
-from .version import __version__
-
 monkey.patch_all()
+
+import pitop.common.ptdm  # noqa: E402
+import zmq.green  # noqa: E402
+
+from .version import __version__  # noqa: E402
+
 # Use zmq.green for gevent compatibility
 pitop.common.ptdm.zmq = zmq.green
