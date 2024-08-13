c6f8ca7470b7dc0e75b9c725cc3cfd159a7492ff
Jorge Capona <jcapona@gmail.com>
2024-08-13 10:39:17 -0400

    Mock std library on web-portal main script

--- pt_os_web_portal/__init__.py
+++ pt_os_web_portal/__init__.py
@@ -1,11 +1,1 @@
-from gevent import monkey
-
-monkey.patch_all()
-
-import pitop.common.ptdm  # noqa: E402
-import zmq.green  # noqa: E402
-
 from .version import __version__  # noqa: E402
-
-# Use zmq.green for gevent compatibility
-pitop.common.ptdm.zmq = zmq.green
