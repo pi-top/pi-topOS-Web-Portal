37f05ebdee4743c06b450ca30ba498dedf7946f4
Jorge Capona <jcapona@gmail.com>
2025-03-14 13:33:44 -0300

    Add cache control headers to index.html and backend response
    - Added meta tags for cache control in index.html to prevent caching.
    - Implemented a no-cache response header in the Flask app to ensure fresh content delivery.

--- pt_os_web_portal/backend/__init__.py
+++ pt_os_web_portal/backend/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from os import environ
 
 from flask import Flask
@@ -11,6 +12,18 @@ def create_app(test_mode, os_updater):
     app = Flask(
         __name__, static_url_path="", static_folder="./build", template_folder="./build"
     )
+
+    @app.after_request
+    def no_cache(response):
+        response.headers["Cache-Control"] = (
+            "no-store, no-cache, must-revalidate, max-age=0"
+        )
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "0"
+        response.headers["Last-Modified"] = datetime.now()
+        response.headers.pop("Etag", None)
+        return response
+
     global sockets
     sockets = Sockets(app)
 
